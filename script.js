6093d6f01134b08857bb8c3ab90f0c1a68dddb3e
MaciejRaku <m.rakowski1987@gmail.com>
2017-02-20 12:58:20 +0100

    1
    1

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-$(document).ready(function() {
+alert('test');
+
+
+/*
+$(document).ready(function(){
+
+  $('li').hover(
+    function(){
+    $(this).addClass('activeHover')
+    },
+    function(){
+    $(this).removeClass('activeHover')    
+    }
+  );
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function() {
    $('div').mouseenter(function() {
        $(this).animate({
            height: '+=10px'
@@ -8,8 +37,10 @@ $(document).ready(function() {
        $(this).animate({
            height: '-=10px'
        }); 
+
    });
    $('div').click(function() {
        $(this).toggle(1000);
    }); 
 });
+*/
